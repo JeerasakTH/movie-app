8f0d4d0c5f4029e884da30d7d03e5f8773ce0112
Jeerasak Todsungnern <78092831+JeerasakTH@users.noreply.github.com>
2021-12-07 13:47:27 +0700

    Clean code, Create Detail

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,31 +1,18 @@
 import React, { useEffect } from 'react';
 import MovieListing from "../MovieListing/MovieListing";
-import movieApi from "../../common/apis/movieApi";
-import { APIKey } from "../../common/apis/MovieApiKey";
 
-import { addMovies } from '../../features/movies/movieSlice';
+
 import { useDispatch } from 'react-redux';
+import { fetchAsyncMovies, fetchAsyncShows, } from '../../features/movies/movieSlice';
 
 const Home = () => {
     
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const movieText = "Harry";
-        const fetchMovies = async () => {
-                const response = await movieApi
-                .get(
-                    `?apiKey=${APIKey}&s=${movieText}&type=movie`
-                  )
-                .catch((err) => {
-                    console.log("Err :", err);
-                });
-            dispatch(addMovies(response.data));
-        };
-        
-
-        fetchMovies();
-    }, []);
+        dispatch(fetchAsyncMovies());
+        dispatch(fetchAsyncShows());
+    }, [dispatch]);
     
     return (
         <div>
